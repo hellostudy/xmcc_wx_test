e86dc3b5b108f67155112ed294fe94aec8639e6c
xmcc <1368816029@qq.com>
2019-06-20 21:21:11 +0800

    第二次微信支付提交到git

--- src/main/java/com/xmcc/WxTestApplication.java
+++ src/main/java/com/xmcc/WxTestApplication.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class WxTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WxTestApplication.class, args);
+        SpringApplication.run(WxTestApplication.class, args) ;
     }
 
 }
